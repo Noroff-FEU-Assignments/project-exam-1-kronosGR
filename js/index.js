27588e218e92d2c30218870d879e2cd98d732d22
KronosGR <geo.elgeo@gmail.com>
2021-04-21 12:22:41 +0200

    v0.1.2 Home page finished

--- js/index.js
+++ js/index.js
@@ -7,30 +7,30 @@ const previous = document.querySelector("#previous");
 const circle1 = document.querySelector("#circle1");
 const circle2 = document.querySelector("#circle2");
 const circle3 = document.querySelector("#circle3");
-const learningList = document.querySelector(".learning-list")
+const learningList = document.querySelector(".learning-list");
 
 var page = 1;
 let res;
 let posts;
 
-next.addEventListener("click", () => {
+next.addEventListener("click", (e) => {
   page = page < 3 ? (page += 1) : 3;
-  const childWidth = carouselInner.offsetWidth;
+  const childWidth = carousel.offsetWidth - (carousel.offsetWidth / 100) * 4;
   carouselInner.scrollBy({
     top: 0,
     left: childWidth,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   checkTheCircles();
 });
 previous.addEventListener("click", () => {
   page = page > 1 ? (page -= 1) : 1;
-  const childWidth = carouselInner.offsetWidth;
+  const childWidth = carousel.offsetWidth - (carousel.offsetWidth / 100) * 5;
   carouselInner.scrollBy({
     top: 0,
     left: -childWidth,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   checkTheCircles();
 });
 
@@ -40,25 +40,25 @@ circle1.addEventListener("click", () => {
   carouselInner.scrollBy({
     top: 0,
     left: -childWidth * 3,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   checkTheCircles();
 });
 circle2.addEventListener("click", () => {
   const childWidth = carouselInner.offsetWidth;
   let offset = 0;
-  if (page ==1) {
-    offset = childWidth
-  } else if (page ==3){
-    offset = -childWidth
+  if (page == 1) {
+    offset = childWidth - (carousel.offsetWidth / 100) * 3;
+  } else if (page == 3) {
+    offset = -childWidth + (carousel.offsetWidth / 100) * 4;
   }
 
   page = 2;
   carouselInner.scrollBy({
     top: 0,
     left: offset,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   checkTheCircles();
 });
 circle3.addEventListener("click", () => {
@@ -67,15 +67,18 @@ circle3.addEventListener("click", () => {
   carouselInner.scrollBy({
     top: 0,
     left: childWidth * 3,
-    behavior: 'smooth'
-  })
+    behavior: "smooth",
+  });
   checkTheCircles();
 });
-
+window.addEventListener("resize", () => {
+  console.log("a");
+  adjustSize();
+});
 
 showPage();
 
-async function showPage(){
+async function showPage() {
   res = await fetchAllProduct(12, "desc");
   posts = await getMedia();
   showCarousel();
@@ -85,35 +88,46 @@ async function showPage(){
 /**
  * add the pages to the carousel
  */
-function showCarousel(){
+function showCarousel() {
+  carouselInner.innerHTML = "";
   carouselInner.appendChild(printCarouselPages(1, posts));
   carouselInner.appendChild(printCarouselPages(2, posts));
   carouselInner.appendChild(printCarouselPages(3, posts));
+  carouselInner.innerHTML += "<div class='after'></div>";
+  adjustSize();
+}
 
-  const childWidth = carouselInner.offsetWidth;
+/**
+ * adjusts the size for the carousel items
+ */
+function adjustSize() {
+  const childWidth = carousel.offsetWidth;
   const children = document.querySelectorAll(".carousel-item");
-  children.forEach(child => {
+  children.forEach((child) => {
     child.style.width = childWidth / 16 / 2.1 + "rem";
-  })
+  });
 }
 
 /**
  * creates the pages for the carousel
  * @param {int} pag current page
  * @param {array} arr posts to print
- * @returns 
+ * @returns
  */
 function printCarouselPages(pag, arr) {
   const pageSize = 4;
   let start = 0 + (pag * pageSize - pageSize);
-  
+
   const p = document.createElement("div");
+  p.id = "page" + pag;
   p.classList.add("carousel-child");
   for (let i = start; i < start + pageSize; i++) {
     p.innerHTML += `
-    <a href="" class="carousel-item">
+    <a href="tutorial.html?id=${arr[i].id}" class="carousel-item">
       <img src=${arr[i].thumb} alt="${arr[i].title}">
-      <h2>${arr[i].title}</h2>
+      <div>
+        <h2>${arr[i].title}</h2>
+      </div>
     </a>
     `;
   }
@@ -127,7 +141,7 @@ function printCarouselPages(pag, arr) {
 async function getMedia() {
   let results = res.map((post) => {
     return fetchMediaWithUrl(post["_links"]["wp:featuredmedia"][0].href).then(
-      (images) => {      
+      (images) => {
         const thumb = images["media_details"].sizes.medium.source_url;
         const title = post.title.rendered;
         const postId = post.id;
@@ -164,20 +178,19 @@ function checkTheCircles() {
 /**
  * Shows the start learning list
  */
-async function fillStartLearning(){
+async function fillStartLearning() {
   const learningPosts = await fetchAllProduct(6, "asc");
 
-  learningPosts.forEach(post => {
+  learningPosts.forEach((post) => {
     const learningItem = document.createElement("div");
-    learningItem.classList.add("learning-item")
+    learningItem.classList.add("learning-item");
 
-    let shortDesc = post.content.rendered.substring(0,100)+ "...";
+    let shortDesc = post.content.rendered.substring(0, 100) + "...";
     learningItem.innerHTML = `<h3>${post.title.rendered}</h3>
     <p>${shortDesc}</p><br><br>
-    <a href="">Read</a>
-    `
+    <a href="tutorial.html?id=${post.id}">Read</a>
+    `;
 
     learningList.appendChild(learningItem);
-  })
-  console.log(learningPosts);
+  });
 }
---
+++ js/tutorials.js
@@ -0,0 +1,1 @@
+import {fetchAllProduct} from './be.js';
