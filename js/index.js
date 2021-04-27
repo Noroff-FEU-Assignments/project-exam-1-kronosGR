84ab6552824e267904cc96b885bf3922cd5d5eca
KronosGR <geo.elgeo@gmail.com>
2021-04-27 11:25:09 +0200

    v0.5.4

--- js/index.js
+++ js/index.js
@@ -8,24 +8,36 @@ const circle1 = document.querySelector("#circle1");
 const circle2 = document.querySelector("#circle2");
 const circle3 = document.querySelector("#circle3");
 const learningList = document.querySelector(".learning-list");
+const prevDiv = document.querySelector(".left-big");
+const nextDiv = document.querySelector(".right-big");
 
 var page = 1;
 let res;
 let posts;
+// carousel animation
+let moving = false;
 
 checkIfLoggedIn(document.querySelector(".account-image"));
 
-next.addEventListener("click", (e) => {
-  page = page < 3 ? (page += 1) : 3;
-  const childWidth = carousel.offsetWidth - (carousel.offsetWidth / 100) * 4;
-  carouselInner.scrollBy({
-    top: 0,
-    left: childWidth,
-    behavior: "smooth",
-  });
-  checkTheCircles();
+next.addEventListener("click", e => {
+  moveToNext();
 });
+
+nextDiv.addEventListener("click", e => {
+  moveToNext();
+});
+
 previous.addEventListener("click", () => {
+  moveToPrev();
+});
+
+prevDiv.addEventListener("click", () => {
+  moveToPrev();
+});
+
+function moveToPrev() {
+  if (moving) return;
+  moving = true;
   page = page > 1 ? (page -= 1) : 1;
   const childWidth = carousel.offsetWidth - (carousel.offsetWidth / 100) * 5;
   carouselInner.scrollBy({
@@ -34,9 +46,36 @@ previous.addEventListener("click", () => {
     behavior: "smooth",
   });
   checkTheCircles();
-});
+  if (page == 1)
+    carouselInner.scrollBy({
+      top: 0,
+      left: -childWidth * 3,
+      behavior: "smooth",
+    });
+}
+
+function moveToNext() {
+  if (moving) return;
+  moving = true;
+  page = page < 3 ? (page += 1) : 3;
+  const childWidth = carousel.offsetWidth - (carousel.offsetWidth / 100) * 4;
+  carouselInner.scrollBy({
+    top: 0,
+    left: childWidth,
+    behavior: "smooth",
+  });
+  checkTheCircles();
+  if (page == 3)
+    carouselInner.scrollBy({
+      top: 0,
+      left: childWidth * 3,
+      behavior: "smooth",
+    });
+}
 
 circle1.addEventListener("click", () => {
+  if (moving) return;
+  moving = true;
   page = 1;
   const childWidth = carouselInner.offsetWidth;
   carouselInner.scrollBy({
@@ -47,6 +86,8 @@ circle1.addEventListener("click", () => {
   checkTheCircles();
 });
 circle2.addEventListener("click", () => {
+  if (moving) return;
+  moving = true;
   const childWidth = carouselInner.offsetWidth;
   let offset = 0;
   if (page == 1) {
@@ -64,6 +105,8 @@ circle2.addEventListener("click", () => {
   checkTheCircles();
 });
 circle3.addEventListener("click", () => {
+  if (moving) return;
+  moving = true;
   page = 3;
   const childWidth = carouselInner.offsetWidth;
   carouselInner.scrollBy({
@@ -104,9 +147,10 @@ function showCarousel() {
 function adjustSize() {
   const childWidth = carousel.offsetWidth;
   const children = document.querySelectorAll(".carousel-item");
-  children.forEach((child) => {
+  children.forEach(child => {
     child.style.width = childWidth / 16 / 2.1 + "rem";
   });
+  circle1.click();
 }
 
 /**
@@ -140,15 +184,13 @@ function printCarouselPages(pag, arr) {
  * @returns An array
  */
 async function getMedia() {
-  let results = res.map((post) => {
-    return fetchMediaWithUrl(post["_links"]["wp:featuredmedia"][0].href).then(
-      (images) => {
-        const thumb = images["media_details"].sizes.medium.source_url;
-        const title = post.title.rendered;
-        const postId = post.id;
-        return { title: title, thumb: thumb, id: postId };
-      }
-    );
+  let results = res.map(post => {
+    return fetchMediaWithUrl(post["_links"]["wp:featuredmedia"][0].href).then(images => {
+      const thumb = images["media_details"].sizes.medium.source_url;
+      const title = post.title.rendered;
+      const postId = post.id;
+      return { title: title, thumb: thumb, id: postId };
+    });
   });
   return Promise.all(results);
 }
@@ -174,6 +216,11 @@ function checkTheCircles() {
       circle3.src = "/images/circle.png";
       break;
   }
+
+  // give some time for the animation to complete.
+  setTimeout(() => {
+    moving = false;
+  }, 1000);
 }
 
 /**
@@ -182,7 +229,7 @@ function checkTheCircles() {
 async function fillStartLearning() {
   const learningPosts = await fetchAllPosts(6, "asc");
 
-  learningPosts.forEach((post) => {
+  learningPosts.forEach(post => {
     const learningItem = document.createElement("div");
     learningItem.classList.add("learning-item");
 
